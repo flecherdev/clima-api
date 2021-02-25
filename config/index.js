eedb2501b8e26203b7beb1c53f982f347775d3f6
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-02-25 11:46:58 -0300

    [feature] added config and env

---
+++ config/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const config = {
+    dev: process.env.NODE_ENV !== 'production',
+    key_openWeather: process.env.KEY_OPENWEATHER,
+    url_openWeather: process.env.URL_OPENWEATHER
+}
+
+module.exports = { config }
