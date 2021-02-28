71a0d9e0db78e4dfe70bfacb536d64f8d9f047e3
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-02-28 15:15:55 -0300

    [fix] added cors

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/', (req, res, next) => {
     res.send('Hello')
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 weatherApiRouter(app)
 
 const server = app.listen(config.port ? config.port : 3000, () => {
